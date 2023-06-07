790ad9224e71b70b935b8a474db6049230d20a51
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-07 19:12:42 +0200

    edit request store

--- app/Http/Requests/StoreProjectRequest.php
+++ app/Http/Requests/StoreProjectRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreProjectRequest extends FormRequest
 {
@@ -24,7 +25,8 @@ class StoreProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            'title' => 'required | min:5 | max:200 | unique:projects',
+            // 'title' => 'required | min:5 | max:200 | unique:projects',
+            'title' => ['required', 'min:5', 'max:200', Rule::unique('projects', 'title')->ignore($this->project)],
             'image' => 'required | min:5',
             'link_ghit' => 'required | min:5',
             'link_site' => 'required | min:5',
