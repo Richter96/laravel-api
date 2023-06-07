98a19db271fe29a39076749ca343997ce376fcfb
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-07 16:17:01 +0200

    add migration foreign key

--- database/migrations/2023_06_07_140543_add_type_id_foreign_key_to_projects_table.php
+++ database/migrations/2023_06_07_140543_add_type_id_foreign_key_to_projects_table.php
@@ -14,7 +14,10 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            // va aggiunto un campo per indicare quale è la colonna dell'id
+            $table->unsignedBigInteger('type_id')->nullable()->after('id'); //mettiamo nullable perche ci possono essere dei campi nel quale non ce il type. AFTER per indicare che la vogliamo dopo un elemento
+            // vincolo, indicando alla chiave esterna sopra creata a quale colonna di quale tabella fa parte. indichiamo cosa succede se il record venisse eliminato possiamo dire di settare tutti gli elementi con questa categoria su null.
+            $table->foreign('type_id')->references('id')->on('types')->onDelete(('set null'));
         });
     }
 
