9ee9acad00cbeb10dcbfdae33a719a15451b551d
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-07 22:45:53 +0200

    add template index type

--- resources/views/admin/type/index.blade.php
+++ resources/views/admin/type/index.blade.php
@@ -3,24 +3,37 @@
 
 @section('content')
 <div class="container-md">
-    <div class="row justify-content-center align-items-center g-2">
+    <div class="row justify-content-center g-2 py-4">
         <div class="col">
-            <form action="{{route('admin.types.store')}}">
+            <form action="{{route('admin.types.store',)}}" method="post">
                 @csrf
                 <div class="input-group mb-3">
-                    <button class="btn btn-outline-secondary" type="submit" id=" ">Add</button>
-                    <input type="text" class="form-control" placeholder="" aria-label="Add" aria-describedby="">
+                    <span class="input-group-text" id="name">NOME</span>
+                    <input type="text" class="form-control" placeholder="inserisci nome type" aria-label="Username" aria-describedby="name">
                 </div>
-
                 <div class="input-group mb-3">
-                    <input type="text" class="form-control" placeholder="" aria-label="Button" aria-describedby="">
-                    <button class="btn btn-outline-secondary" type="button" id="">Button</button>
+                    <span class="input-group-text" id="image">LINK IMMAGINE</span>
+                    <input type="text" class="form-control" placeholder="link immagine" aria-label="link immagine" aria-describedby="image">
                 </div>
-
+                <button type="submit" class="btn btn-light">Add</button>
             </form>
         </div>
         <div class="col">
-
+            <div class="container">
+                <div class="row row-cols-2 g-4">
+                    @foreach ($types as $type)
+                    <div class="col d-flex">
+                        <div class="card">
+                            <img class="card-img-top" src="{{$type->image}}" alt="{{$type->name}}">
+                            <div class="card-body">
+                                <h4 class="card-title">{{$type->name}}</h4>
+                                <a name="" id="" class="btn btn-info" href="{{route('admin.types.edit', $type)}}" role="button"><i class="fas fa-pencil fa-sm fa-fw"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
         </div>
     </div>
 </div>
