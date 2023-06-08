09e34a1df5aeb5b49541347f70e14b6f9b7b1334
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-06-08 16:59:31 +0200

    create a seedr for Technologies

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +16,12 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['php', 'laravel', 'bootstrap', 'css', 'scss', 'js', 'programming'];
+
+        foreach ($technologies as $technology) {
+            $newtechnology = new Technology();
+            $newtechnology->name = $technology;
+            $newtechnology->slug = Str::slug($newtechnology->name, '-');
+        }
     }
 }
